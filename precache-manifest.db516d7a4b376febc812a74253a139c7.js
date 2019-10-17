2df9a8b36f872d104054276dbe3a5d93eee695e6
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-10-17 16:25:42 -0400

    Updates

---
+++ precache-manifest.db516d7a4b376febc812a74253a139c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c878e9a86651778abef56e7f447e66",
+    "url": "/reading-rainbo-gh/index.html"
+  },
+  {
+    "revision": "2e2b7a309ecee5ede2ca",
+    "url": "/reading-rainbo-gh/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "393a387f0452bd8a5588",
+    "url": "/reading-rainbo-gh/static/js/2.0941efac.chunk.js"
+  },
+  {
+    "revision": "2e2b7a309ecee5ede2ca",
+    "url": "/reading-rainbo-gh/static/js/main.45d28318.chunk.js"
+  },
+  {
+    "revision": "445c9441dd8f07176b19",
+    "url": "/reading-rainbo-gh/static/js/runtime-main.e05a874f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reading-rainbo-gh/static/media/logo.25bf045c.svg"
+  }
+]);
